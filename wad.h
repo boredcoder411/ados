b5a0b33f2e344dc1c1c6a7b370fbd2b503cbe0f2
boredcoder411 <taamarcos@gmail.com>
2025-01-17 22:45:51 +0100

    useless

--- wad.h
+++ wad.h
@@ -1,11 +1,6 @@
 #pragma once
 
 #include <stdint.h>
-#include <stdlib.h>
-
-void list_wad_files(const char* wad_file_path);
-int save_to_file(const char* file_path, const uint8_t* data, size_t size);
-void show_wad_file(const char* wad_file_path, const char* lump_name);
 
 // Struct for WAD header
 typedef struct {
